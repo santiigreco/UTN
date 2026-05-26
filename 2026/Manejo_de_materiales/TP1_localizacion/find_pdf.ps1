71284db53f1b40d71c8db8ad8c2b7184ab26183f
unknown <santigreco@hotmail.com>
2026-05-26 20:35:39 -0300

    proyecto

--- 2026/Manejo_de_materiales/TP1_localizacion/find_pdf.ps1
+++
@@ -1,4 +0,0 @@
-$files = Get-ChildItem 'd:\Descargas\UTN\Repo-UTN\2026\Manejo_de_materiales\TP1_localizacion\*.pdf'
-foreach ($f in $files) {
-    Write-Output "PDF: $($f.FullName)"
-}
--- 2026/Manejo_de_materiales/TP1_localizacion/list_files.ps1
+++
@@ -1,4 +0,0 @@
-$files = Get-ChildItem 'd:\Descargas\UTN\Repo-UTN\2026\Manejo_de_materiales\TP1_localizacion'
-foreach ($f in $files) {
-    Write-Output $f.FullName
-}
